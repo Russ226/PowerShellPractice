87fd625754a45b43441376f09ee3fb3097c2cda8
Ruslan Radetskiy <russ2510@gmail.com>
2019-05-04 15:25:34 -0400

    basic file delete with command line args

--- DeletingOldfiles.ps1
+++ DeletingOldfiles.ps1
@@ -1,16 +1,19 @@
 # delete files in dir based on path
 
 param(
-    [String]$Path
-
+    [String]$Path,
+    [String]$Days
 )
 
-if(Test-Path $Path -Container){
-    $Daysback = "-30"
- 
+
+
+if(Test-Path $Path -PathType Container){
+
     $CurrentDate = Get-Date
-    $DatetoDelete = $CurrentDate.AddDays($Daysback)
+    $DatetoDelete = $CurrentDate.AddDays($Days)
     Get-ChildItem $Path | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
+
+    
 
 }
 
